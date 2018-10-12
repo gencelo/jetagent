bc627ed0879c0fbe09092dc486da9fd97a6f71eb
gencelo <genceloguzhan@gmail.com>
2018-10-12 22:34:42 +0300

    Initialize project

---
+++ src/main/java/com/oguzhan/jetagent/JetagentApplication.java
@@ -0,0 +1,12 @@
+package com.oguzhan.jetagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JetagentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JetagentApplication.class, args);
+    }
+}
